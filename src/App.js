0d73c59fc5f90e3132372e2b79b863e10457a5db
keysi.fernandes <keysi.fernandes@hypeflame.com.br>
2021-10-22 09:49:35 -0300

    feat: adiciona imagens dos projetos desenvolvidos e link para o whats

--- src/App.js
+++ src/App.js
@@ -1,145 +1,76 @@
 import "./App.css";
-import "@splidejs/splide/dist/css/splide.min.css";
-import { library } from "@fortawesome/fontawesome-svg-core";
-import Splide from "@splidejs/splide";
-import frete from "./fotos/frete.jpeg";
-// import frete2 from "./fotos/frete2.jpeg";
-import carro from "./fotos/carro.jpeg";
-import {
-  fab,
-  faLinkedin,
-  // faFacebook,
-  // faGithub,
-  faWhatsapp,
-} from "@fortawesome/free-brands-svg-icons";
-import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
-import { useEffect } from "react";
+// import {
+// fab,
+// faLinkedin,
+// } from "@fortawesome/free-brands-svg-icons";
+// import { faCheckSquare, faCoffee } from "@fortawesome/free-solid-svg-icons";
 // import axios from "axios";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import keysiPhoto from "./fotos/keysi-contorno.png";
+import institutoAdmin from "./fotos/instituto-admin.png";
+import instipoa from "./fotos/instipoa.png";
+import jccarretos from "./fotos/jccarretos.png";
 
-library.add(fab, faLinkedin, faCheckSquare, faCoffee);
-
+// library.add(fab, faLinkedin, faCheckSquare, faCoffee);
+const openWhatsApp = () =>
+  window.location.replace("https://wa.me/5551993667706?text=Oi Keysi Jones =D");
 function App() {
-  useEffect(() => {
-    new Splide(".splide").mount();
-  }, []);
-  // axios
-  //   .get("https://api.github.com/users/KeysiJones")
-  //   .then((res) => console.log(res));
   return (
-    <div className="md:text-center text-left">
-      <div id="header" className="py-4 bg-red-400">
-        <p className="text-2xl text-black pl-4 text-center shadow-3xl">
-          JC CARRETOS{" "}
-          <a
-            className="underline"
-            href="https://wa.me/5551996524349/?text=Oi, tudo bem ? eu gostaria de fazer um orçamento de frete."
+    <div className="bg-yellow-300 h-screen">
+      <div className="bg-yellow-300 p-2">
+        <div>
+          <p
+            style={{ textShadow: "#f3b80c -2px 4px 0px" }}
+            className="text-gray-600 font-bold pt-4 text-center text-4xl"
           >
-            <FontAwesomeIcon icon={faWhatsapp} size="1x" />
-          </a>
-        </p>
-        <p className="text-center text-white text-xl">
-          Qualidade e confiança tem nome
-        </p>
-      </div>
-      <div className="p-4 mt-4">
-        <p className="text-2xl font-thin text-center">
-          Aqui na JC Carretos <b>nos importamos com você</b> e com seus
-          pertences.
-        </p>
-        <div
-          id="image-slider"
-          className="splide text-center m-auto inline-block mt-8"
-        >
+            Meu nome é Jones... <br /> Keysi Jones !
+          </p>
+        </div>
+        <div>
+          <img
+            className="m-auto rounded-full"
+            alt="Foto de um jovem de 25 anos, de camiseta preta de manga curta e óculos preto"
+            src={keysiPhoto}
+          />
           <div
-            className="splide__track"
-            style={{ textAlign: "-webkit-center" }}
+            id="main-buttons"
+            className="flex m-4 flex-col text-center justify-center"
           >
-            <ul className="splide__list">
-              <li className="splide__slide">
-                <img
-                  alt="caminhonete preta com móvel na parte de trás"
-                  src={frete}
-                  className="rounded"
-                ></img>
-              </li>
-              {/* <li className="splide__slide">
-                <img src={frete2} className="rounded"></img>
-              </li> */}
-              <li className="splide__slide">
-                <img
-                  alt="caminhonete com móvel na parte de trás"
-                  src={carro}
-                  className="rounded"
-                ></img>
-              </li>
-            </ul>
+            <button
+              onClick={() => window.location.replace("#")}
+              className="p-4 m-4 bg-red-400 text-white rounded-xl"
+            >
+              <p className="font-bold">Meus projetos</p>
+            </button>
+            <button
+              onClick={openWhatsApp}
+              className="p-4 m-4 bg-red-400 text-white rounded-xl"
+            >
+              <p className="font-bold">Fale comigo</p>
+            </button>
           </div>
-
-          <div className="splide__progress">
-            <div className="splide__progress__bar"></div>
+          <div className="m-4">
+            <div className="pb-8 px-4">
+              <a href="https://jccarretos.vercel.app/">
+                <img className="rounded-md" alt="project" src={jccarretos} />
+              </a>
+            </div>
+            <div className="pb-8 px-4">
+              <a href="https://instituto-helper.netlify.app/">
+                <img className="rounded-md" alt="project" src={instipoa} />
+              </a>
+            </div>
+            <div className="pb-8 px-4">
+              <a href="https://github.com/KeysiJones/instituto-admin">
+                <img
+                  className="rounded-md"
+                  alt="project"
+                  src={institutoAdmin}
+                />
+              </a>
+            </div>
           </div>
         </div>
-        <p className="text-2xl mt-4 font-thin text-center">
-          Fazemos <b>pequenos fretes/carretos</b> em Porto Alegre e no litoral
-          gaúcho.
-        </p>
-        <p className="text-2xl mt-4 font-thin text-center">
-          Não fica no aperto, chama a JC e{" "}
-          <a
-            className="font-bold"
-            href="https://wa.me/5551980179373/?text=Oi, tudo bem ? eu gostaria de fazer um orçamento de frete."
-          >
-            AGENDA AGORA
-          </a>{" "}
-          o teu carreto !
-        </p>
-        <p className="text-2xl mt-4 font-thin text-center">
-          E para agendar <b>é muito fácil</b>, é só tocar no ícone do whats aqui
-          em baixo.
-        </p>
-
-        <p className="text-2xl mt-4 font-thin text-center">
-          Agendar um carreto nunca foi tão fácil.
-        </p>
-        <p className="text-2xl mt-4 font-thin text-center">
-          <a
-            className="underline"
-            href="https://wa.me/5551996524349/?text=Oi, tudo bem ? eu gostaria de fazer um orçamento de frete."
-          >
-            <FontAwesomeIcon icon={faWhatsapp} size="2x" />
-          </a>{" "}
-        </p>
-        <p className="text-2xl mt-4 font-thin text-center">
-          Whatsapp: (51){" "}
-          <a
-            className="underline"
-            href="https://wa.me/5551996524349/?text=Oi, tudo bem ? eu gostaria de fazer um orçamento de frete."
-          >
-            996524349{" "}
-          </a>
-          /{" "}
-          <a
-            className="underline"
-            href="https://wa.me/5551980179373/?text=Oi, tudo bem ? eu gostaria de fazer um orçamento de frete."
-          >
-            980179373
-          </a>
-        </p>
-      </div>
-      <div id="footer" className="py-4 bg-red-400">
-        <p className="text-center text-white text-xl mb-2">
-          Qualidade e confiança tem nome
-        </p>
-        <p className="text-2xl text-black pl-4 text-center shadow-3xl">
-          JC CARRETOS{" "}
-          <a
-            className="underline"
-            href="https://wa.me/5551996524349/?text=Oi, tudo bem ? eu gostaria de fazer um orçamento de frete."
-          >
-            <FontAwesomeIcon icon={faWhatsapp} size="1x" />
-          </a>
-        </p>
       </div>
     </div>
   );
