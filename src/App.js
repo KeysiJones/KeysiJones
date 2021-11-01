ffb95e37231a0555f2e41f3393200c6c6697c1be
keysi.fernandes <keysi.fernandes@hypeflame.com.br>
2021-11-01 16:19:44 -0300

    feat: melhora tamanho da imagem e disposicao dos itens

--- src/App.js
+++ src/App.js
@@ -8,12 +8,12 @@ import {
 import institutoAdmin from "./fotos/instituto-admin.png";
 import instipoa from "./fotos/instipoa.png";
 import jccarretos from "./fotos/jccarretos.png";
-import thug from "./fotos/keysiredglasses.png";
+import thug from "./fotos/keysiredsquare.png";
 import { useEffect, useState } from "react";
 import Switch from "react-switch";
 import Splide from "@splidejs/splide";
 import "@splidejs/splide/dist/css/splide.min.css";
-import thugBlack from "./fotos/keysibordered.png";
+import thugBlack from "./fotos/keysiblacksquare.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const openLink = (url) => window.open(url, "_blank");
@@ -30,14 +30,15 @@ function App() {
     <div className={`${darkMode ? "bg-gray-800" : "bg-white"} h-screen`}>
       <div className={`${darkMode ? "bg-gray-800" : "bg-white"} p-2`}>
         <div className="blink">
-          <div className="flex flex-col gap-1 rounded-t-3xl" id="aboutme">
-            <img
-              id="thug"
-              className="m-auto mt-2 -mb-8"
-              alt="Foto de um jovem de 25 anos, de camiseta preta de manga curta e óculos preto"
-              src={darkMode ? thugBlack : thug}
-            />
-
+          <div className="flex flex-col md:flex-row gap-1" id="aboutme">
+            <div className="mx-auto">
+              <img
+                id="thug"
+                className="mt-2 -mb-8"
+                alt="Foto de um jovem de 25 anos, de camiseta preta de manga curta e óculos preto"
+                src={darkMode ? thugBlack : thug}
+              />
+            </div>
             <div id="myinfo" className="rounded-3xl">
               <div className="self-center mb-4">
                 <label className="text-right m-auto text-sm">
@@ -56,21 +57,23 @@ function App() {
                   />
                 </label>
               </div>
-              <h1
-                className={`${
-                  darkMode ? "text-white" : "text-gray-700"
-                } font-semibold text-center text-5xl md:text-6xl mx-2`}
-              >
-                Hi, i'm Keysi Jones
-              </h1>
+              <div>
+                <h1
+                  className={`${
+                    darkMode ? "text-white" : "text-gray-700"
+                  } font-semibold text-center text-5xl md:text-6xl mx-2`}
+                >
+                  Hi, i'm Keysi Jones
+                </h1>
 
-              <p
-                className={`${
-                  darkMode ? "text-gray-300" : "text-gray-600"
-                } text-center font-semibold text-2xl md:text-3xl lg:text-4xl mx-2 mt-2`}
-              >
-                Frontend Web Developer
-              </p>
+                <p
+                  className={`${
+                    darkMode ? "text-gray-300" : "text-gray-600"
+                  } text-center font-semibold text-2xl md:text-3xl lg:text-4xl mx-2 mt-2`}
+                >
+                  Frontend Developer
+                </p>
+              </div>
 
               <div
                 id="main-buttons"
@@ -105,7 +108,7 @@ function App() {
           <h2
             className={`${
               darkMode ? "text-white" : "text-gray-700"
-            } font-bold py-4 text-center text-4xl sm:text-5xl`}
+            } font-bold py-4 text-center text-4xl sm:text-5xl my-8`}
           >
             My projects
           </h2>
