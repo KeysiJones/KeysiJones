2d4c4c207aa86fdf8f1253cb4eb1c07170591dc8
kfernand <keysi.fernandes@adp.com>
2022-02-05 19:59:59 -0300

    feat: update main pic and improve ui

--- src/components/carrousel-card.js
+++ src/components/carrousel-card.js
@@ -10,7 +10,7 @@ function CarrouselCard({ cardImage, link, description, openLink }) {
         <h3
           className={`text-2xl ${
             darkMode ? "text-gray-600" : "text-gray-700"
-          } text-center pb-4 m-2 font-semibold`}
+            } text-center pb-4 m-2 font-semibold`}
         >
           {description}
         </h3>
@@ -21,7 +21,7 @@ function CarrouselCard({ cardImage, link, description, openLink }) {
               darkMode
                 ? "bg-gray-700 text-green-400"
                 : "bg-green-500 text-white"
-            }  rounded-b-xl w-full`}
+              }  rounded-b-xl w-full`}
           >
             <p className="font-bold text-4xl mb-4">Live demo</p>
           </button>
