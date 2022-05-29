c439ed30992d17c4bfa0ccecc5c1bbc1dbcce53e
keysijones <keysi.fernandes@clevertech.biz>
2022-05-28 21:22:28 -0300

    Feat: makes prof pic bigger

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -17,11 +17,7 @@ function AboutMe() {
         <div className="mx-auto">
           <img
             id="thug"
-            className={`mt-8 ${
-              darkMode
-                ? "bg-gradient-to-br from-yellow-400 to-cyan-500"
-                : "bg-gradient-to-br from-yellow-400 to-yellow-300"
-            } rounded-full`}
+            className="rounded-full"
             alt="Foto de um jovem de 25 anos, de camiseta preta de manga curta e óculos preto"
             src={avatarImagePath}
           />
@@ -30,41 +26,23 @@ function AboutMe() {
           <div id="myinfo" className="rounded-3xl">
             <div>
               <h1 className="font-semibold text-center text-3xl md:text-4xl mx-auto">
-                {window.innerWidth < 452 ? (
+                <span
+                  className={`${darkMode ? "text-white" : "text-gray-800"}`}
+                >
+                  Hi, i'm{" "}
                   <span
-                    className={`${darkMode ? "text-white" : "text-gray-700"}`}
-                  >
-                    Hi, i'm{" "}
-                    <span
-                      className={`${
-                        darkMode ? "text-yellow-400" : "text-yellow-400"
+                    className={`${darkMode ? "text-yellow-400" : "text-yellow-400"
                       }`}
-                    >
-                      Keysi
-                    </span>
-                  </span>
-                ) : (
-                  <span
-                    className={`${darkMode ? "text-white" : "text-gray-700"}`}
                   >
-                    Hi, i'm{" "}
-                    <span
-                      className={`${
-                        darkMode ? "text-yellow-400" : "text-yellow-400"
-                      }`}
-                    >
-                      Keysi Jones
-                    </span>
+                    Jones
                   </span>
-                )}
+                </span>
               </h1>
-
               <p
-                className={`${
-                  darkMode ? "text-white" : "text-gray-700"
-                } text-center font-semibold text-2xl lg:text-4xl mx-2 mt-2`}
+                className={`${darkMode ? "text-white" : "text-gray-800"
+                  } text-center font-semibold text-2xl lg:text-4xl mx-2 mt-2`}
               >
-                <span>Fullstack Javascript</span> Engineer (MERN)
+                <span>Fullstack Javascript</span> Engineer
               </p>
             </div>
             {window.innerWidth >= 375 ? <ContactCard /> : null}
