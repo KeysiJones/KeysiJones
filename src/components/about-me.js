486ce2fe092761e17f2bcdfc2500347a4d611d52
keysijones <keysi.fernandes@clevertech.biz>
2022-05-29 21:23:50 -0300

    Feat: ux improvements

--- src/components/about-me.js
+++ src/components/about-me.js
@@ -7,6 +7,8 @@ function AboutMe() {
   const darkMode = theme === "dark";
   const GRAVATAR_HASH = process.env.REACT_APP_GRAVATAR_HASH;
   const avatarImagePath = `https://www.gravatar.com/avatar/${GRAVATAR_HASH}?s=280`;
+  const isLargeScreen = window.innerWidth >= 1024;
+  const isMobile = window.innerWidth <= 375;
 
   return (
     <div>
@@ -14,38 +16,40 @@ function AboutMe() {
         <div className="self-end mb-4 mr-8" id="switch-small">
           <ThemeSwitcher />
         </div>
-        <div className="mx-auto">
+        <div className={`lg:flex ${darkMode && isLargeScreen && "bg-gray-700"} ${!darkMode && isLargeScreen && "bg-gray-200"} py-4`}>
           <img
             id="thug"
-            className="rounded-full"
+            className="rounded-full mx-auto"
             alt="Foto de um jovem de 25 anos, de camiseta preta de manga curta e óculos preto"
             src={avatarImagePath}
           />
-        </div>
-        <div className="flex flex-col self-center m-auto sm:mt-12">
-          <div id="myinfo" className="rounded-3xl">
-            <div>
-              <h1 className="font-semibold text-center text-3xl md:text-4xl mx-auto">
-                <span
-                  className={`${darkMode ? "text-white" : "text-gray-800"}`}
-                >
-                  Hi, i'm{" "}
+          <div className="flex flex-col self-center m-auto sm:mt-12">
+            <div id="myinfo" className="rounded-3xl">
+              <div>
+                <h1 className="font-semibold text-center text-3xl md:text-4xl mx-auto">
                   <span
-                    className={`${darkMode ? "text-yellow-400" : "text-yellow-400"
-                      }`}
+                    className={`${darkMode ? "text-white" : "text-gray-700"}`}
                   >
-                    Jones
+                    Hi, i'm{" "}
+                    <span
+                      className={`${
+                        darkMode ? "text-yellow-400" : "text-yellow-500"
+                      }`}
+                    >
+                      Jones
+                    </span>
                   </span>
-                </span>
-              </h1>
-              <p
-                className={`${darkMode ? "text-white" : "text-gray-800"
+                </h1>
+                <p
+                  className={`${
+                    darkMode ? "text-white" : "text-gray-700"
                   } text-center font-semibold text-2xl lg:text-4xl mx-2 mt-2`}
-              >
-                <span>Fullstack Javascript</span> Engineer
-              </p>
+                >
+                  <span>Fullstack Javascript</span> Engineer
+                </p>
+              </div>
+              {!isMobile && <ContactCard />}
             </div>
-            {window.innerWidth >= 375 ? <ContactCard /> : null}
           </div>
         </div>
       </div>
