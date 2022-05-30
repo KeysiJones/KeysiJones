f8e8654cd570221fc42fe31033fcc3aff2bc19d1
keysijones <keysi.fernandes@clevertech.biz>
2022-05-29 21:23:50 -0300

    Feat: ux improvements

--- src/components/section-title.js
+++ src/components/section-title.js
@@ -2,7 +2,7 @@ function SectionTitle({ darkMode, title }) {
   return (
     <h2
       className={`${
-        darkMode ? "text-yellow-400" : "text-yellow-400"
+        darkMode ? "text-yellow-400" : "text-yellow-500"
       } font-bold text-center text-3xl my-8`}
     >
       {title}
