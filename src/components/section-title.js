2266e82b6a0719a9550b528c02ae2e2d8155dd56
Jones Fernandes <jones.fernandes@clevertech.biz>
2022-08-12 23:47:40 -0300

    removes useless code, uses better font sizes

--- src/components/section-title.js
+++ src/components/section-title.js
@@ -1,6 +1,6 @@
 function SectionTitle({ title }) {
   return (
-    <h2 className='my-16 text-5xl font-bold text-center text-yellow-400 sm:text-6xl lg:text-[85px] lg:my-28'>
+    <h2 className='my-16 text-5xl font-bold text-center text-yellow-400 sm:text-6xl lg:text-[80px] lg:my-28'>
       {title}
     </h2>
   );
