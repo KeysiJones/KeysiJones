83298f0fd72648cfa4b15c774f04d2770fcdbd58
Jones Fernandes <jones.fernandes@clevertech.biz>
2022-08-12 23:16:18 -0300

    cleans and improves both carousel and hero section components

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,25 +1,25 @@
-import {
-  faLinkedin,
-  faTwitter,
-  faGithub,
-  faTelegram,
-} from '@fortawesome/free-brands-svg-icons';
+// import {
+//   faLinkedin,
+//   faTwitter,
+//   faGithub,
+//   faTelegram,
+// } from '@fortawesome/free-brands-svg-icons';
 
-import { ContactCard } from './';
+// import { ContactCard } from './';
 
 function HeroSection() {
   const GRAVATAR_HASH = process.env.REACT_APP_GRAVATAR_HASH;
   const avatarImagePath = `https://www.gravatar.com/avatar/${GRAVATAR_HASH}?s=280`;
-  const icons = {
-    linkedin: faLinkedin,
-    github: faGithub,
-    twitter: faTwitter,
-    telegram: faTelegram,
-  };
+  // const icons = {
+  //   linkedin: faLinkedin,
+  //   github: faGithub,
+  //   twitter: faTwitter,
+  //   telegram: faTelegram,
+  // };
 
   return (
     <div
-      className='flex h-screen py-4 lg:items-center lg:px-40 lg:flex lg:flex-row'
+      className='flex h-[96vh] py-4 lg:items-center lg:px-40 lg:flex lg:flex-row'
       id='hero-section'
     >
       <div
@@ -31,17 +31,18 @@ function HeroSection() {
           className='w-48 m-auto rounded-full sm:w-48 md:w-60'
           alt="Keysi Jones's avatar"
         />
-        <p className='mb-12 text-center text-white text-7xl md:text-8xl lg:text-9xl'>
+        <p className='mt-8 mb-12 text-center text-white text-7xl md:text-8xl lg:text-9xl'>
           I make the <span className='text-yellow-400'>web</span> a better place
           for <span className='text-yellow-400'>you</span>
         </p>
         <p className='mx-2 mt-2 text-2xl font-light text-center text-white lg:text-4xl'>
-          by building <span className='text-yellow-400'>fullstack</span> apps
+          by building <span className='text-yellow-400'>beautiful </span> and{' '}
+          <span className='text-yellow-400'>reliable</span> applications
         </p>
-        {/* <button className='p-4 m-auto mt-10 text-2xl bg-yellow-400 hero rounded-xl'>
+        <button className='p-4 m-auto text-2xl bg-yellow-400 mt-14 hero rounded-xl'>
           Get in touch now
-        </button> */}
-        <ContactCard icons={icons} footer={true} socialMediaIconsSize='lg' />
+        </button>
+        {/* <ContactCard icons={icons} footer={true} socialMediaIconsSize='lg' /> */}
       </div>
     </div>
   );
